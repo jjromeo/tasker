8439c098d44d29b0b8aee806be7187e2c4b35300
Jerome Pratt <jerome.pratt@outlook.com>
2014-12-26 12:39:01 +0000

    added ability for hash to be initialized with certain attributes

--- lib/task.rb
+++ lib/task.rb
@@ -1,6 +1,10 @@
 class Task 
 
-  def initialize
+  attr_accessor :action, :time_required
+
+  def initialize(data_hash = {})
+    @action = data_hash[:action]
+    @time_required = data_hash[:time_required]
     @completed = false
   end
 
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,14 +1,20 @@
 describe Task do 
 
+  let(:task) {Task.new}
+
   it 'on initialization is not completed' do 
-    task = Task.new
     expect(task).not_to be_completed
   end
 
   it 'can be completed' do 
-    task = Task.new 
     task.mark_as_completed
     expect(task).to be_completed
+  end
+
+  it 'can be initialized with certain attributes' do 
+    task2 = Task.new(action:"tidy my room", time_required: 20)
+    expect(task2.action).to eq "tidy my room"
+    expect(task2.time_required).to eq 20
   end
 
 end
