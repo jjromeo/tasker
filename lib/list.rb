e09db04afc88242dd574d9143b06981c51042289
Jerome Pratt <jerome.pratt@outlook.com>
2014-12-28 17:21:18 +0000

    list can now only add tasks to list and can mark tasks as completed

--- lib/list.rb
+++ lib/list.rb
@@ -5,10 +5,22 @@ class List
   end
 
   def add_task(task)
-    tasks << task
+    if task.class == Task
+      tasks << task 
+    else 
+      raise "Only tasks can be added to the list"
+    end
   end
 
   def todo 
+    tasks.reject { |task| task.completed?}
+  end
+
+  def finished 
     tasks.select { |task| task.completed?}
+  end
+
+  def tick_off(task)
+    task.mark_as_completed
   end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,7 +1,7 @@
 describe List do 
   let(:list) {List.new}
-  let (:task) { double "task", completed?: false }
-  let (:completed_task) { double "task", completed?: true}
+  let (:task) { double "task", completed?: false, mark_as_completed: false, class: Task }
+  let (:completed_task) { double "task", completed?: true, class: Task}
 
   it 'does not contain any tasks on initialization' do 
     expect(list.tasks.count).to eq 0
@@ -12,11 +12,29 @@ describe List do
     expect(list.tasks.count).to eq 1
   end
 
+  it 'can only add tasks to the list' do 
+    non_task = double "non_task", class: "NotTask"
+    expect{list.add_task(non_task)}.to raise_error "Only tasks can be added to the list"
+  end
+
   it 'can list its tasks which are not yet completed' do 
     list.add_task(task) 
     list.add_task(completed_task) 
     expect(list.tasks.count).to eq 2
     expect(list.todo.count).to eq 1
+  end
+
+  it 'can list its completed tasks' do 
+    list.add_task(task) 
+    list.add_task(completed_task) 
+    expect(list.tasks.count).to eq 2
+    expect(list.finished.count).to eq 1
+  end
+
+  it 'can mark tasks as completed' do 
+    list.add_task(task)
+    expect(task).to receive(:mark_as_completed)
+    list.tick_off(task)
   end
 
 end
